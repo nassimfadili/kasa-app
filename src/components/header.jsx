4b6c99c7536936f7f7f33709f3145450f5f97c59
Nassim <nassimfadili@outlook.fr>
2024-06-29 18:30:21 +0200

    scss arangement

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "../logo/logoKASA.png";
 import "../style/App.scss";
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <header>
-                <nav className="Navbar">
-                    <img src={Logo} alt="logo" />
-                    <li>
-                        <ul>
-                            Accueil
-                        </ul>
-                        <ul>
-                            A Propos
-                        </ul>
+            <nav className="Navbar">
+                <img src={Logo} alt="logo" />
+                <ul>
+                    <li onClick={() => handleNavigation("/")}>
+                        Accueil
                     </li>
-                </nav>
+                    <li onClick={() => handleNavigation("/a-propos")}>
+                        A Propos
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
