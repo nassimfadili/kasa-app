32f38ab29b71bbe49915b28e1dfcb434072dd452
Nassim <nassimfadili@outlook.fr>
2024-07-15 12:19:04 +0200

    debbug final

--- src/components/logement.jsx
+++ src/components/logement.jsx
@@ -1,39 +1,42 @@
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const Appart = ({title, name, picture,location, tag, rating}) => {
-    return <div>
-
-        <div className='info-content'>
-            <div className='titleUser'>
-                <h2 className='appartTitle'>{title}</h2>
-                <div className='NamePicture'>
-                    <p className='userName'>{name}</p>
-                    <img className='userPicture' src= {picture} alt= {picture}/>
-                </div>
-            </div>
-            <div className='location'>
-                <p>{location}</p>
-            </div>
-            <div className='tagStar'>
-                <div className='TagContent'>
+const Appart = ({ title, name, picture, location, tag, rating }) => {
+  return (
+    <div className="LogementContent">
+      <div className="info-content">
+        <div className="titleUser">
+          <h2 className="appartTitle">{title}</h2>
+        </div>
+        <div className="location">
+          <p>{location}</p>
+        </div>
+        <div className="tagStar">
+          <div className="TagContent">
             {tag.map((tag, index) => (
-            <p key={index}>{tag}</p>
-          ))}
-          </div>
-          <div className='stars'>
-          {[...Array(5)].map((_, index) => (
-              <FontAwesomeIcon
-                key={index}
-                icon={faStar}
-                className={`star ${index < rating ? 'filled' : ''}`}
-              />
+              <p key={index}>{tag}</p>
             ))}
           </div>
         </div>
+      </div>
+      <div className="StarsAndName">
+        <div className="NamePicture">
+          <p className="userName">{name}</p>
+          <img className="userPicture" src={picture} alt={picture} />
+        </div>
+        <div className="stars">
+          {[...Array(5)].map((_, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              className={`star ${index < rating ? "filled" : ""}`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-    </div>
-}
+  );
+};
 
-export default Appart
+export default Appart;
